44625ecd5024c9e98f75efff2c32164fe514ba37
Darius Merlin Arbabha <Darius Merlin Arbabha>
2024-06-19 13:28:14 +0200

    improved prompts (spelling, coherent and nbgrader MC format)

--- prompts.py
+++ prompts.py
@@ -1,6 +1,6 @@
 
 FREE_TEXT_QUESTIONS = """
-Create exam questions based for the topic : {topic}
+Create exam questions based on the topic : {topic}
 
 Lecture content:
 {content}
@@ -8,35 +8,40 @@ Lecture content:
 Generate {num_questions} questions based on the content above, make sure they are relevant and challenging.
 
 DO NOT INTRODUCE ANY QUESTIONS THAT ARE NOT BASED ON THE CONTENT ABOVE
-THE GENERTED QUESTIONS SHOULD NOT ASKED ABOUT RESEARCH PAPERS OR AUTHORS
+THE GENERATED QUESTIONS SHOULD NOT ASK ABOUT RESEARCH PAPERS OR AUTHORS
 NEVER PROVIDE AN ANSWER
 """
 
 
 CODE_QUESTIONS = """
-Create exam questions based for the topic : {topic}
+Create one exam question based on the topic : {topic}
 
 Lecture content:
 {content}
+
 Generate a single coding question based on the content above, make sure it is relevant and challenging.
+
 YOU MUST ALWAYS PROVIDE A CODE SNIPPET IN THE QUESTION AND ASK A QUESTION BASED ON IT 
-THE QUESTIONS SHOULD NOT REQUIERE ANY ACTUAL CODING, JUST UNDERSTANDING OF THE CODE
+THE QUESTIONS SHOULD NOT REQUIRE ANY ACTUAL CODING, JUST UNDERSTANDING OF THE CODE
 NEVER PROVIDE AN ANSWER
 """
 
 MC_QUESTIONS = """
-Create exam questions based for the topic : {topic}
+Create one exam question based on the topic : {topic}
 
 Lecture content:
 {content}
 
 Generate a single multiple choice question based on the content above, make sure it is relevant and challenging.
-THE QUESTION SHOULD HAVE 4 POSSIBLE ANSWERS, DO NOT PROVIDE MORE OR LESS, NO ANSWER SHOULD BE PRPOVIDED
+
+THE QUESTION SHOULD HAVE 4 POSSIBLE ANSWERS, DO NOT PROVIDE MORE OR LESS, NO ANSWER SHOULD BE PROVIDED
 NEVER PROVIDE IRRELEVANT OPTIONS
 
 OUTPUT FORMAT:
-A) Answer 1
-B) Answer 2
-C) Answer 3
-D) Answer 4
+### QUESTION
+
+- Answer 1
+- Answer 2
+- Answer 3
+- Answer 4
 """
